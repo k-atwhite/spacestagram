bebafb403bf954a2b40dba588cd83e820dd9f8b5
Kat <kat.white.a@gmail.com>
2021-09-19 15:18:54 -0600

    Add heart to each card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,6 +4,9 @@ import './Card.css';
 const Card = ({ title, image, rover, earthDate, camera, id }) => {
   return (
     <div className="card">
+      <span>
+        ​ <i class="fas fa-heart"></i>
+      </span>
       <img src={image} alt={title} className="image"></img>
       <p>Rover: {rover}</p>
       <p>Date taken: {earthDate}</p>
