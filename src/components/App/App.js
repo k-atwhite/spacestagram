fbd909f8e455d0b649304e49ef1f6e396f84fd3b
Kat <kat.white.a@gmail.com>
2021-09-19 16:24:55 -0600

    Add functions for liking and unliking

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,25 @@ import Cards from '../Cards/Cards';
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
-  const [error, setError] = useState('');
+  const [likedPhotos, setLiked] = useState([]);
+  // const [error, setError] = useState('');
 
   useEffect(() => {
     fetchLatestPhotos().then((data) => setPhotos(data.latest_photos));
   }, []);
+
+  const likePhoto = (card) => {
+    setLiked([card, ...likedPhotos]);
+  };
+
+  const unlikePhoto = () => {};
 
   return (
     <div className="App">
       <header className="header">
         <h1>SPACESTAGRAM</h1>
       </header>
-      <Cards data={photos} />
+      <Cards data={photos} likePhoto={likePhoto} unlikePhoto={unlikePhoto} />
     </div>
   );
 };
