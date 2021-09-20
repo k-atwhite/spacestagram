64ccf16d94cfaa7ca944a89eb39b9a537bb0c534
Kat <kat.white.a@gmail.com>
2021-09-19 22:12:31 -0600

    Pass all tests

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -7,35 +7,36 @@ describe('Homepage', () => {
     cy.get('h1').contains('SPACESTAGRAM');
   });
 
-  it('Should show cards with an image and description and a like-heart', () => {
-    cy.get('card').first().contains('img');
-    cy.get('card').first().contains('.description');
-    cy.get('card').first().contains('.fa-heart');
-  });
-
   it('Should show a button for favorites and a button for all', () => {
-    cy.get('button').eq(2);
+    cy.get('.fav-btn').should('be.visible');
+    cy.get('.all-btn').should('be.visible');
   });
 
-  it('Clicking the like heart should change the color of the heart to red', () => {
-    cy.get('.card').first().contains('.fa-heart').click().get('favorite');
+  it('Should show cards with an image and description and a like-heart', () => {
+    cy.get('img').first().should('be.visible');
+    cy.get('.description').should('be.visible');
+    cy.get('.fa-heart').first().should('be.visible');
   });
 
   it('The favorites button should show no images to start', () => {
-    cy.get('.fav-btn').click().get('.card').should('not.be.visible');
+    cy.get('.fav-btn').click().get('.card').should('not.exist');
+  });
+
+  it('Clicking the like heart should change the color of the heart to red', () => {
+    cy.get('.fa-heart').first().click().get('.favorite');
   });
 
   it('After adding a favorite, clicking the my favorites button should show all favorites', () => {
     cy.get('.fa-heart').first().click();
-    cy.get('fav-button').click().get('card').should('be.visible');
+    cy.get('.fav-btn').click().get('.card').should('have.length', 1);
   });
 
   it('Clicking the heart a second time should change the color and remove the image as a favorite', () => {
     cy.get('.fa-heart').first().click();
-    cy.get('fav-button').click().get('card').should('not.be.visible');
+    cy.get('.fav-btn').click().get('card').should('not.exist');
   });
 
   it('Clicking the show all button should return to all images', () => {
-    // cy.get('all-button').click()
+    // cy.get('all-button').click();
   });
 });
